dbbc9235971905d1d532298f3785fbeb7dd5dd76
lina.wolf <lwolf@w-commerce.de>
2023-07-10 11:49:42 +0000

    [BUGFIX] Load TypoScript

---
+++ ext_localconf.php
@@ -0,0 +1,9 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+if (!defined('TYPO3')) {
+    die('Access denied.');
+}
+
+ExtensionManagementUtility::addTypoScriptSetup('@import \'EXT:form_autocomplete/Configuration/TypoScript/setup.typoscript\'');
